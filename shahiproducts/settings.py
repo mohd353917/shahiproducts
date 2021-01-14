051d26a5d403df3b39749547c7427781c6b2d6da
Mohd. Hassan <mh728646@gmail.com>
2021-01-14 21:07:52 +0530

    added DATABASE_URL and runtime.txt

--- shahiproducts/settings.py
+++ shahiproducts/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "") == "true"
+DEBUG = os.getenv("DEBUG", "") == os.getenv("DEBUG", "false")
 
 ALLOWED_HOSTS = ["*"] if DEBUG else []
 
@@ -74,12 +76,7 @@ WSGI_APPLICATION = "shahiproducts.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {"default": dj_database_url.config(conn_max_age=600)}
 
 
 # Password validation
